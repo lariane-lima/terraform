b75c5b657f83105ae1bd4575fb198af78f599c8a
lariane <lariane_costalima@hotmail.com>
2022-03-29 10:27:44 -0300

    commit inicial

---
+++ maint.tf
@@ -0,0 +1,22 @@
+terraform {
+    required_providers {
+      aws = {
+          source = "hashicorp/aws"
+          version = "~> 3.0.0"
+      }
+    }
+    required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+    region = "us-east-1" 
+}
+
+resource "aws_instance" "primeira-instancia" {
+    ami = "ami-0e472ba40eb589f49"
+    instance_type = "t2.micro"
+    key_name = "terraform-aws"
+    tags = {
+        Name = "primeira-instancia"
+    }
+}
